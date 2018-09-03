c46cb1552bf2fe252d169d16d68f12ac358f8fd4
Certil Remy <certil@noukod.com>
2018-09-03 15:38:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,15 @@
 def display_board(board)
 
 end
+
+def valid_move?(board , index)
+
+  if  board[index] == " " || ""
+    return false
+    
+  end
+elsif  board[index] == "O" || "X"
+  return true
+  
+  
+end
